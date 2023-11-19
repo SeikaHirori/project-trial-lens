eca1042e884238fddf653896cf6da85e2717f7b0
SeikaHirori <SeikaHirori@outlook.com>
2023-11-19 10:42:30 -0800

    ADD: Determining Refractive Error Type

--- src/Model/TrialLens.js
+++ src/Model/TrialLens.js
@@ -10,21 +10,30 @@ var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (
     if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot read private member from an object whose class did not declare it");
     return kind === "m" ? f : kind === "a" ? f.call(receiver) : f ? f.value : state.get(receiver);
 };
-var _TrialLens_instances, _TrialLens_currentPatient, _TrialLens_refractiveErrorType, _TrialLens_determineRefractiveErrorType;
+var _TrialLens_currentPatient, _TrialLens_refractiveErrorType;
 Object.defineProperty(exports, "__esModule", { value: true });
+const RefractiveErrorType_1 = require("./RefractiveErrorType");
 class TrialLens {
     constructor(patient) {
-        _TrialLens_instances.add(this);
         _TrialLens_currentPatient.set(this, void 0);
         _TrialLens_refractiveErrorType.set(this, void 0);
         __classPrivateFieldSet(this, _TrialLens_currentPatient, patient, "f");
-        __classPrivateFieldSet(this, _TrialLens_refractiveErrorType, __classPrivateFieldGet(this, _TrialLens_instances, "m", _TrialLens_determineRefractiveErrorType).call(this, patient), "f"); // FIXME ; This is placeholder... Use function "determineRefractiveErrorType() when it is written out."
+        __classPrivateFieldSet(this, _TrialLens_refractiveErrorType, this.determineRefractiveErrorType(patient), "f"); // FIXME ; This is placeholder... Use function "determineRefractiveErrorType() when it is written out."
+    }
+    // // TODO
+    determineRefractiveErrorType(patient) {
+        const patientSphere = patient.sphereRaw;
+        if (patientSphere < 0) {
+            return new RefractiveErrorType_1.Myopia(__classPrivateFieldGet(this, _TrialLens_currentPatient, "f"));
+        }
+        else if (patientSphere === 0) {
+            return new RefractiveErrorType_1.Emmetropic(__classPrivateFieldGet(this, _TrialLens_currentPatient, "f"));
+        }
+        else if (patientSphere > 0) {
+            return new RefractiveErrorType_1.Hyperopic(__classPrivateFieldGet(this, _TrialLens_currentPatient, "f"));
+        }
+        throw new Error("Unable to determine Refractive Error Type");
     }
 }
-_TrialLens_currentPatient = new WeakMap(), _TrialLens_refractiveErrorType = new WeakMap(), _TrialLens_instances = new WeakSet(), _TrialLens_determineRefractiveErrorType = function _TrialLens_determineRefractiveErrorType(patient) {
-    const patientSphere = patient.sphereRaw;
-    if (patientSphere < 0) {
-        return;
-    }
-};
+_TrialLens_currentPatient = new WeakMap(), _TrialLens_refractiveErrorType = new WeakMap();
 //# sourceMappingURL=TrialLens.js.map
