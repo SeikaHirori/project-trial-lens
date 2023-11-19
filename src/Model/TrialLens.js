7c32be92c354edfdf4c2f524a5bd8799639bcc06
SeikaHirori <SeikaHirori@outlook.com>
2023-11-19 10:42:30 -0800

    ADD: Determining Refractive Error Type

--- src/Model/TrialLens.js
+++ src/Model/TrialLens.js
@@ -5,14 +5,34 @@ var __classPrivateFieldSet = (this && this.__classPrivateFieldSet) || function (
     if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot write private member to an object whose class did not declare it");
     return (kind === "a" ? f.call(receiver, value) : f ? f.value = value : state.set(receiver, value)), value;
 };
+var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (receiver, state, kind, f) {
+    if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a getter");
+    if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot read private member from an object whose class did not declare it");
+    return kind === "m" ? f : kind === "a" ? f.call(receiver) : f ? f.value : state.get(receiver);
+};
 var _TrialLens_currentPatient, _TrialLens_refractiveErrorType;
 Object.defineProperty(exports, "__esModule", { value: true });
+const RefractiveErrorType_1 = require("./RefractiveErrorType");
 class TrialLens {
     constructor(patient) {
         _TrialLens_currentPatient.set(this, void 0);
         _TrialLens_refractiveErrorType.set(this, void 0);
         __classPrivateFieldSet(this, _TrialLens_currentPatient, patient, "f");
-        // this.#refractiveErrorType = this.#determineRefractiveErrorType(patient); // FIXME ; This is placeholder... Use function "determineRefractiveErrorType() when it is written out."
+        __classPrivateFieldSet(this, _TrialLens_refractiveErrorType, this.determineRefractiveErrorType(patient), "f"); // FIXME ; This is placeholder... Use function "determineRefractiveErrorType() when it is written out."
+    }
+    // // TODO
+    determineRefractiveErrorType(patient) {
+        const patientSphere = patient.sphereRaw;
+        if (patientSphere < 0) {
+            return new RefractiveErrorType_1.Myopia(__classPrivateFieldGet(this, _TrialLens_currentPatient, "f"));
+        }
+        else if (patientSphere === 0) {
+            return new RefractiveErrorType_1.Emmetropic(__classPrivateFieldGet(this, _TrialLens_currentPatient, "f"));
+        }
+        else if (patientSphere > 0) {
+            return new RefractiveErrorType_1.Hyperopic(__classPrivateFieldGet(this, _TrialLens_currentPatient, "f"));
+        }
+        throw new Error("Unable to determine Refractive Error Type");
     }
 }
 _TrialLens_currentPatient = new WeakMap(), _TrialLens_refractiveErrorType = new WeakMap();
--- src/Model/TrialLens.ts
+++ src/Model/TrialLens.ts
@@ -1,5 +1,5 @@
 import { Patient } from "./Patient";
-import { RefractiveErrorType, Emmetropic, Hyperopic, Myopia } from "./RefractiveErrorType";
+import { RefractiveErrorType, debugConcreteRefractiveErrorType, Emmetropic, Hyperopic, Myopia } from "./RefractiveErrorType";
 
 class TrialLens {
     #currentPatient: Patient;
@@ -7,16 +7,22 @@ class TrialLens {
 
     constructor(patient: Patient) {
         this.#currentPatient = patient;
-        // this.#refractiveErrorType = this.#determineRefractiveErrorType(patient); // FIXME ; This is placeholder... Use function "determineRefractiveErrorType() when it is written out."
+        this.#refractiveErrorType = this.determineRefractiveErrorType(patient); // FIXME ; This is placeholder... Use function "determineRefractiveErrorType() when it is written out."
     }
 
     // // TODO
-    // #determineRefractiveErrorType(patient: Patient): RefractiveErrorType {
-    //     const patientSphere: number = patient.sphereRaw;
+    determineRefractiveErrorType(patient: Patient): RefractiveErrorType {
+        const patientSphere: number = patient.sphereRaw;
 
-    //     if (patientSphere < 0) {
-    //         return 
-    //     }
-    // }
+        if (patientSphere < 0) {
+            return new Myopia(this.#currentPatient);
+        } else if (patientSphere === 0) {
+            return new Emmetropic(this.#currentPatient);
+        } else if (patientSphere > 0) {
+            return new Hyperopic(this.#currentPatient);
+        }
+
+        throw new Error("Unable to determine Refractive Error Type")
+    }
 }
 
