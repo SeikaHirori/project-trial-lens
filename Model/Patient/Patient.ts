987c9ad11c1f810eeb5732e887745b66819d0881
SeikaHirori <SeikaHirori@outlook.com>
2024-04-04 14:51:41 -0700

    Removed empty line

--- Model/Patient/Patient.ts
+++ Model/Patient/Patient.ts
@@ -15,7 +15,6 @@ export class Patient {
         this.#valueAfterFactoringAstigmatism = this.calculateTotalAfterAstigmatism(sphere, cylinder);
     }
 
-
     get sphereRaw(): number {
         return this.#sphere;
     }
