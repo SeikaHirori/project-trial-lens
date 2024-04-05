39865d0f4fb241cbddec10cfa80c845728a3c00f
SeikaHirori <SeikaHirori@outlook.com>
2024-04-05 13:11:15 -0700

    Made UserAnswer a class

--- src/Model/UserAnswer.ts
+++ src/Model/UserAnswer.ts
@@ -1,4 +1,19 @@
-export interface UserAnswer {
-    answer: number
-    isTrialLensNotNeeded: boolean
+import { aN } from "vitest/dist/reporters-P7C2ytIv.js"
+
+export default class UserAnswer {
+    readonly #answer: number 
+    readonly #isTrialLensNotNeeded: boolean
+
+    constructor(answer: number, isTrialLensNotNeeeded: boolean){
+        this.#answer = answer * 100 // Convert parameter to be non-decimal value
+        this.#isTrialLensNotNeeded = isTrialLensNotNeeeded
+    }
+
+    get rawAnswer() {
+        return this.#answer
+    }
+
+    get formattedAnswer() {
+        return this.#answer / 100
+    }
 }
