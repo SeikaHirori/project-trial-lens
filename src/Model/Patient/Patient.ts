9631d482337f1ddc0040f91b2f8e555907df287a
SeikaHirori <SeikaHirori@outlook.com>
2024-07-12 12:26:46 -0700

    "Patient" object: Added equals && JSDocs (TODO)

--- src/Model/Patient/Patient.ts
+++ src/Model/Patient/Patient.ts
@@ -13,30 +13,53 @@ export class Patient {
         this.#age = age;
     }
 
-    get sphereRaw(): number {
+    /**
+     * @returns {number} - raw, unformatted value
+     */
+    get sphere(): number {
         return this.#sphere;
     }
 
+    /**
+     * @returns {number} - formatted into decimal
+     */
     get sphereFormatted(): number {
         return this.#sphere / 100;
     }
 
-    get cylinderRaw(): number {
+    /**
+     * @returns {number} - raw, unformatted value
+     */
+    get cylinder(): number {
         return this.#cylinder;
     }
 
+    /**
+     * @returns {number} - formatted into decimal
+     */
     get cylinderFormatted(): number {
         return this.#cylinder / 100;
     }
 
+    /**
+     * @returns {number}
+     */
     get axis(): number {
         return this.#axis;
     }
 
+    /**
+     * @returns {number}
+     */
     get age(): number {
         return this.#age;
     }
 
-
-
+    equals(that: Patient): boolean {
+        return this.sphere === that.sphere
+            && this.cylinder === that.cylinder
+            && this.axis === that.axis
+            && this.age === that.age;
+    }
+    
 }
