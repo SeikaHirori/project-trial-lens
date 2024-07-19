481063bf87cae274073bdcc3e216912cd4248c77
SeikaHirori <SeikaHirori@outlook.com>
2024-07-19 12:36:26 -0700

    Implemented RNG

--- src/Controller/Random.ts
+++ src/Controller/Random.ts
@@ -1,13 +1,20 @@
 
 /**
- * Source for generating random numbers:
+ * Source for generating random numbers in range within JavaScript/ TypeScript:
  * https://www.freecodecamp.org/news/generate-random-number-within-a-range-in-javascript/
  * 
  * Note: JavaScript/ TypeScript does not have a built-in random number 
  * generator between ranges.
  */
 const getRandom = (min, max) => {
+    const floatRandom = Math.random()
+    const difference = max - min
 
+    // random between 0 and the difference
+    const random = Math.round(difference * floatRandom);
+    const randomWithinRange = random + min;
+
+    return randomWithinRange;
 }
 
 export default getRandom
